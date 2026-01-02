2f6e0438e261b3fad332a3674fa757d07d6866ec
wendy218126 <wendy900218@gmail.com>
2026-01-02 23:24:01 +0800

    Create check.py

---
+++ check.py
@@ -0,0 +1,21 @@
+import requests
+import os
+
+URL = "https://shop.weverse.io/en/shop/USD/artists/3/sales/43782"
+WEBHOOK = os.environ["DISCORD_WEBHOOK"]
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+html = requests.get(URL, headers=headers).text
+
+if "SOLD OUT" not in html:
+    requests.post(
+        WEBHOOK,
+        json={
+            "content": "🚨 **Weverse 補貨啦！**\n" + URL
+        }
+    )
+else:
+    print("Still sold out")
